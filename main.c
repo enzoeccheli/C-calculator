5bddadfdfd9c9a74a0c3375b2d1c57480b3f197a
Enzo Eccheli <enzoeccheli@gmail.com>
2026-05-08 10:38:16 -0300

    feat:adciona  melhorias no código - travando div por 0

--- main.c
+++ main.c
@@ -14,34 +14,38 @@ int main(void){
     while(op!=0){
         printf("---------------\n '+' to sum\n '-' to subt\n '*' to mult\n '/' to div\n '=' to finish\n---------------\n INPUT: ");
         fgets(menu, sizeof(menu), stdin);
-        printf("input a number: ");
-        scanf("%f", &next);
-        getchar();
         c=menu[0];
         if(c=='='){
             op=0;
-            continue;
+            break;
         }
+        printf("input a number: ");
+        scanf("%f", &next);
+        getchar();
         switch (c){
             case '+':
                 total+=next;
-                printf("total (parcial): %f\n", total);
+                printf("total (parcial): %.2f\n", total);
                 break;
             case '-':
                 total-=next;
-                printf("total (parcial): %f\n", total);
+                printf("total (parcial): %.2f\n", total);
                 break;
             case '*':
                 total*=next;
-                printf("total (parcial): %f\n", total);
+                printf("total (parcial): %.2f\n", total);
                 break;
             case '/':
-                total/=next;
-                printf("total (parcial): %f\n", total);
-                break;
+                if(next!=0){
+                    total/=next;
+                    printf("total (parcial): %.2f\n", total);
+                }else{
+                    printf("Div 0, error\n");
+                }
+                    break;
         
             default:
-                printf("invalid");
+                printf("MENU ERROR - INPUT A VALID OP CHAR OR NUMBER\n");
                 break;
         }
     }
