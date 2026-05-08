65d7cf60549f456d236d0ac1e438293066fc4283
Enzo Eccheli <enzoeccheli@gmail.com>
2026-05-08 09:52:54 -0300

    feat: adciona grande parte do código

--- main.c
+++ main.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 
 int main(void){
-    
+    char menu[10];
+    char c='d';
+    int op=1;
+    float next, total;
 
+    
+    printf("input your first number: ");
+    scanf("%f", &total);
+    getchar();    
+    
+    while(op!=0){
+        printf("---------------\n '+' to sum\n '-' to subt\n '*' to mult\n '/' to div\n '=' to finish\n---------------\n INPUT: ");
+        fgets(menu, sizeof(menu), stdin);
+        printf("input a number: ");
+        scanf("%f", &next);
+        getchar();
+        c=menu[0];
+        if(c=='='){
+            op=0;
+            continue;
+        }
+        switch (c){
+            case '+':
+                total+=next;
+                printf("total (parcial): %f\n", total);
+                break;
+            case '-':
+                total-=next;
+                printf("total (parcial): %f\n", total);
+                break;
+            case '*':
+                total*=next;
+                printf("total (parcial): %f\n", total);
+                break;
+            case '/':
+                total/=next;
+                printf("total (parcial): %f\n", total);
+                break;
+        
+            default:
+                printf("invalid");
+                break;
+        }
+    }
+    printf("result: %.2f", total);    
     return 0;
 }
